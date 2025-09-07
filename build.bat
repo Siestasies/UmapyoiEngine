333090afee5977c0ae800fb8f2fda7370b5c2416
Siestasies <bitterhoronigai@gmail.com>
2025-09-07 15:01:13 +0800

    Updated build.bat
    Error handling

--- build.bat
+++ build.bat
@@ -7,10 +7,22 @@ cd build
 
 echo Configuring with CMake...
 cmake .. -G "Visual Studio 17 2022" -A x64
+if %errorlevel% neq 0 (
+    echo CMake configuration failed!
+    pause
+    exit /b 1
+)
 
 echo Building UmapyoiEngine...
 cmake --build . --config Debug
+if %errorlevel% neq 0 (
+    echo Build failed!
+    pause
+    exit /b 1
+)
 
 echo Build complete! Running UmapyoiGame...
 cd Game\Debug
 UmapyoiGame.exe
+
+pause
