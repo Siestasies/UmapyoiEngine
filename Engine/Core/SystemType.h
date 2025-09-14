b5aebc7b11448ce01d289333fa4f5b5a798ffd89
beatsmoon <wai.men.leong@gmail.com>
2025-09-14 12:34:22 +0800

    added base system class

---
+++ Engine/Core/SystemType.h
@@ -0,0 +1,28 @@
+#pragma once
+
+namespace Uma_Engine
+{
+    class ISystem
+    {
+    public:
+        virtual ~ISystem() = default;
+
+        /*!
+            \brief Initialize the system.
+            \details Called once at engine startup.
+        */
+        virtual void Init() = 0;
+
+        /*!
+            \brief Update the system each frame.
+            \param dt Delta time in seconds since last frame.
+        */
+        virtual void Update(float dt) = 0;
+
+        /*!
+            \brief Clean up system resources.
+            \details Called once at engine shutdown.
+        */
+        virtual void Shutdown() = 0;
+    };
+}
