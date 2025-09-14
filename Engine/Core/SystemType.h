96abd6469c30cfc40b85e94a18ef84d1f27ad15b
beatsmoon <wai.men.leong@gmail.com>
2025-09-14 16:11:04 +0800

    Update SystemType.h

--- Engine/Core/SystemType.h
+++ Engine/Core/SystemType.h
@@ -3,36 +3,39 @@
 
 namespace UmapyoiEngine
 {
-    // Base system interface - only requires basic lifecycle methods
-    class ISystem
+    namespace EngineSystem
     {
-    public:
-        virtual ~ISystem() = default;
+        // Base system interface - only requires basic lifecycle methods
+        class ISystem
+        {
+        public:
+            virtual ~ISystem() = default;
 
-        /*!
-            \brief Initialize the system.
-            \details Called once at engine startup.
-        */
-        virtual void Init() = 0;
+            /*!
+                \brief Initialize the system.
+                \details Called once at engine startup.
+            */
+            virtual void Init() = 0;
 
-        /*!
-            \brief Update the system each frame.
-            \param dt Delta time in seconds since last frame.
-        */
-        virtual void Update(float dt) = 0;
+            /*!
+                \brief Update the system each frame.
+                \param dt Delta time in seconds since last frame.
+            */
+            virtual void Update(float dt) = 0;
 
-        /*!
-            \brief Clean up system resources.
-            \details Called once at engine shutdown.
-        */
-        virtual void Shutdown() = 0;
-    };
+            /*!
+                \brief Clean up system resources.
+                \details Called once at engine shutdown.
+            */
+            virtual void Shutdown() = 0;
+        };
 
-    // Optional interface for systems that need window access
-    class IWindowSystem
-    {
-    public:
-        virtual ~IWindowSystem() = default;
-        virtual void SetWindow(GLFWwindow* window) = 0;
-    };
+        // Optional interface for systems that need window access
+        class IWindowSystem
+        {
+        public:
+            virtual ~IWindowSystem() = default;
+            virtual void SetWindow(GLFWwindow* window) = 0;
+        };
+    }
 }
