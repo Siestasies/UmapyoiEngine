29814241a9c82a22da5d0d37a9b3cb2b5ef38e00
jedrekktnh <jedrekleejingwei@gmail.com>
2025-09-26 10:34:45 +0800

    Optimized matrix classes

--- Engine/Math/Matrix.h
+++ Engine/Math/Matrix.h
@@ -154,10 +154,10 @@ namespace Uma_Math
             return Matrix2x2(m_data[0], m_data[1], m_data[2], m_data[3]);
         }
 
-        // Static factory methods
         static constexpr Matrix2x2 identity() noexcept
         {
-            return Matrix2x2(T{1}, T{}, T{}, T{1});
+            return Matrix2x2(T{1}, T{},
+                             T{}, T{1});
         }
 
         static constexpr Matrix2x2 zero() noexcept
@@ -299,10 +299,11 @@ namespace Uma_Math
                              m_data[2], m_data[5], m_data[8]);
         }
 
-        // Static factory methods
         static constexpr Matrix3x3 identity() noexcept
         {
-            return Matrix3x3(T{1}, T{}, T{}, T{}, T{1}, T{}, T{}, T{}, T{1});
+            return Matrix3x3(T{1}, T{}, T{},
+                             T{}, T{1}, T{},
+                             T{}, T{}, T{1});
         }
 
         static constexpr Matrix3x3 zero() noexcept
@@ -438,9 +439,19 @@ namespace Uma_Math
         // Matrix operations
         constexpr T determinant() const noexcept
         {
-            return m_data[0] * (m_data[4] * m_data[8] - m_data[5] * m_data[7]) -
-                    m_data[1] * (m_data[3] * m_data[8] - m_data[5] * m_data[6]) +
-                    m_data[2] * (m_data[3] * m_data[7] - m_data[4] * m_data[6]);
+            // Calculate 2x2 determinants that will be reused
+            T const m00 = m_data[0], m01 = m_data[4], m02 = m_data[8], m03 = m_data[12];
+            T const m10 = m_data[1], m11 = m_data[5], m12 = m_data[9], m13 = m_data[13];
+            T const m20 = m_data[2], m21 = m_data[6], m22 = m_data[10], m23 = m_data[14];
+            T const m30 = m_data[3], m31 = m_data[7], m32 = m_data[11], m33 = m_data[15];
+
+            // Calculate 3x3 cofactors for first row
+            T const c0 = m11 * (m22 * m33 - m23 * m32) - m12 * (m21 * m33 - m23 * m31) + m13 * (m21 * m32 - m22 * m31);
+            T const c1 = m10 * (m22 * m33 - m23 * m32) - m12 * (m20 * m33 - m23 * m30) + m13 * (m20 * m32 - m22 * m30);
+            T const c2 = m10 * (m21 * m33 - m23 * m31) - m11 * (m20 * m33 - m23 * m30) + m13 * (m20 * m31 - m21 * m30);
+            T const c3 = m10 * (m21 * m32 - m22 * m31) - m11 * (m20 * m32 - m22 * m30) + m12 * (m20 * m31 - m21 * m30);
+
+            return m00 * c0 - m01 * c1 + m02 * c2 - m03 * c3;
         }
 
         constexpr Matrix4x4 transpose() const noexcept
@@ -453,10 +464,12 @@ namespace Uma_Math
             );
         }
 
-        // Static factory methods
         static constexpr Matrix4x4 identity() noexcept
         {
-            return Matrix4x4(T{1}, T{0}, T{0}, T{0}, T{0}, T{1}, T{0}, T{0}, T{0}, T{0}, T{1}, T{0}, T{0}, T{0}, T{0}, T{1});
+            return Matrix4x4(T{1}, T{0}, T{0}, T{0},
+                             T{0}, T{1}, T{0}, T{0},
+                             T{0}, T{0}, T{1}, T{0},
+                             T{0}, T{0}, T{0}, T{1});
         }
 
         static constexpr Matrix4x4 zero() noexcept
@@ -487,6 +500,23 @@ namespace Uma_Math
                             lhs.get(2) - rhs.get(2), lhs.get(3) - rhs.get(3));
     }
 
+    template <typename T>
+    constexpr Matrix2x2<T> operator*(const Matrix2x2<T>& lhs, const Matrix2x2<T>& rhs)
+    {
+        const auto& l = lhs.m_data;
+        const auto& r = rhs.m_data;
+
+        return Matrix2x2<T>(
+            // Column 0
+            l[0] * r[0] + l[2] * r[1], // [0,0]
+            l[1] * r[0] + l[3] * r[1], // [1,0]
+
+            // Column 1
+            l[0] * r[2] + l[2] * r[3], // [0,1]
+            l[1] * r[2] + l[3] * r[3]  // [1,1]
+        );
+    }
+
     template <typename T, typename U>
     constexpr Matrix2x2<T> operator*(const Matrix2x2<T>& mat, U scalar)
     {
@@ -505,18 +535,6 @@ namespace Uma_Math
     {
         return Matrix2x2<T>(mat.get(0) / scalar, mat.get(1) / scalar,
                             mat.get(2) / scalar, mat.get(3) / scalar);
-    }
-
-    // Matrix multiplication for Matrix2x2
-    template <typename T>
-    constexpr Matrix2x2<T> operator*(const Matrix2x2<T>& lhs, const Matrix2x2<T>& rhs)
-    {
-        return Matrix2x2<T>(
-            lhs.get(0, 0) * rhs.get(0, 0) + lhs.get(0, 1) * rhs.get(1, 0),
-            lhs.get(0, 0) * rhs.get(0, 1) + lhs.get(0, 1) * rhs.get(1, 1),
-            lhs.get(1, 0) * rhs.get(0, 0) + lhs.get(1, 1) * rhs.get(1, 0),
-            lhs.get(1, 0) * rhs.get(0, 1) + lhs.get(1, 1) * rhs.get(1, 1)
-        );
     }
 
     // Arithmetic operators for Matrix3x3
@@ -540,6 +558,30 @@ namespace Uma_Math
             result.set(i, lhs.get(i) - rhs.get(i));
         }
         return result;
+    }
+
+    template <typename T>
+    constexpr Matrix3x3<T> operator*(const Matrix3x3<T>& lhs, const Matrix3x3<T>& rhs)
+    {
+        const auto& l = lhs.m_data;
+        const auto& r = rhs.m_data;
+
+        return Matrix3x3<T>(
+            // Column 0
+            l[0] * r[0] + l[3] * r[1] + l[6] * r[2], // [0,0]
+            l[1] * r[0] + l[4] * r[1] + l[7] * r[2], // [1,0]
+            l[2] * r[0] + l[5] * r[1] + l[8] * r[2], // [2,0]
+
+            // Column 1
+            l[0] * r[3] + l[3] * r[4] + l[6] * r[5], // [0,1]
+            l[1] * r[3] + l[4] * r[4] + l[7] * r[5], // [1,1]
+            l[2] * r[3] + l[5] * r[4] + l[8] * r[5], // [2,1]
+
+            // Column 2
+            l[0] * r[6] + l[3] * r[7] + l[6] * r[8], // [0,2]
+            l[1] * r[6] + l[4] * r[7] + l[7] * r[8], // [1,2]
+            l[2] * r[6] + l[5] * r[7] + l[8] * r[8]  // [2,2]
+        );
     }
 
     template <typename T, typename U>
@@ -568,23 +610,6 @@ namespace Uma_Math
             result.set(i, mat.get(i) / scalar);
         }
         return result;
-    }
-
-    // Matrix multiplication for Matrix3x3
-    template <typename T>
-    constexpr Matrix3x3<T> operator*(const Matrix3x3<T>& lhs, const Matrix3x3<T>& rhs)
-    {
-        return Matrix3x3<T>(
-            lhs.get(0, 0) * rhs.get(0, 0) + lhs.get(0, 1) * rhs.get(1, 0) + lhs.get(0, 2) * rhs.get(2, 0),
-            lhs.get(0, 0) * rhs.get(0, 1) + lhs.get(0, 1) * rhs.get(1, 1) + lhs.get(0, 2) * rhs.get(2, 1),
-            lhs.get(0, 0) * rhs.get(0, 2) + lhs.get(0, 1) * rhs.get(1, 2) + lhs.get(0, 2) * rhs.get(2, 2),
-            lhs.get(1, 0) * rhs.get(0, 0) + lhs.get(1, 1) * rhs.get(1, 0) + lhs.get(1, 2) * rhs.get(2, 0),
-            lhs.get(1, 0) * rhs.get(0, 1) + lhs.get(1, 1) * rhs.get(1, 1) + lhs.get(1, 2) * rhs.get(2, 1),
-            lhs.get(1, 0) * rhs.get(0, 2) + lhs.get(1, 1) * rhs.get(1, 2) + lhs.get(1, 2) * rhs.get(2, 2),
-            lhs.get(2, 0) * rhs.get(0, 0) + lhs.get(2, 1) * rhs.get(1, 0) + lhs.get(2, 2) * rhs.get(2, 0),
-            lhs.get(2, 0) * rhs.get(0, 1) + lhs.get(2, 1) * rhs.get(1, 1) + lhs.get(2, 2) * rhs.get(2, 1),
-            lhs.get(2, 0) * rhs.get(0, 2) + lhs.get(2, 1) * rhs.get(1, 2) + lhs.get(2, 2) * rhs.get(2, 2)
-        );
     }
 
     // Arithmetic operators for Matrix4x4
@@ -638,30 +663,36 @@ namespace Uma_Math
         return result;
     }
 
-    // Matrix multiplication for Matrix4x4
     template <typename T>
     constexpr Matrix4x4<T> operator*(const Matrix4x4<T>& lhs, const Matrix4x4<T>& rhs)
     {
+        const auto& l = lhs.m_data;
+        const auto& r = rhs.m_data;
+
         return Matrix4x4<T>(
-            lhs.get(0, 0) * rhs.get(0, 0) + lhs.get(0, 1) * rhs.get(1, 0) + lhs.get(0, 2) * rhs.get(2, 0) + lhs.get(0, 3) * rhs.get(3, 0),
-            lhs.get(0, 0) * rhs.get(0, 1) + lhs.get(0, 1) * rhs.get(1, 1) + lhs.get(0, 2) * rhs.get(2, 1) + lhs.get(0, 3) * rhs.get(3, 1),
-            lhs.get(0, 0) * rhs.get(0, 2) + lhs.get(0, 1) * rhs.get(1, 2) + lhs.get(0, 2) * rhs.get(2, 2) + lhs.get(0, 3) * rhs.get(3, 2),
-            lhs.get(0, 0) * rhs.get(0, 3) + lhs.get(0, 1) * rhs.get(1, 3) + lhs.get(0, 2) * rhs.get(2, 3) + lhs.get(0, 3) * rhs.get(3, 3),
+            // Column 0 (elements 0-3)
+            l[0] * r[0] + l[4] * r[1] + l[8] * r[2] + l[12] * r[3],   // [0,0]
+            l[1] * r[0] + l[5] * r[1] + l[9] * r[2] + l[13] * r[3],   // [1,0]
+            l[2] * r[0] + l[6] * r[1] + l[10] * r[2] + l[14] * r[3],  // [2,0]
+            l[3] * r[0] + l[7] * r[1] + l[11] * r[2] + l[15] * r[3],  // [3,0]
 
-            lhs.get(1, 0) * rhs.get(0, 0) + lhs.get(1, 1) * rhs.get(1, 0) + lhs.get(1, 2) * rhs.get(2, 0) + lhs.get(1, 3) * rhs.get(3, 0),
-            lhs.get(1, 0) * rhs.get(0, 1) + lhs.get(1, 1) * rhs.get(1, 1) + lhs.get(1, 2) * rhs.get(2, 1) + lhs.get(1, 3) * rhs.get(3, 1),
-            lhs.get(1, 0) * rhs.get(0, 2) + lhs.get(1, 1) * rhs.get(1, 2) + lhs.get(1, 2) * rhs.get(2, 2) + lhs.get(1, 3) * rhs.get(3, 2),
-            lhs.get(1, 0) * rhs.get(0, 3) + lhs.get(1, 1) * rhs.get(1, 3) + lhs.get(1, 2) * rhs.get(2, 3) + lhs.get(1, 3) * rhs.get(3, 3),
+            // Column 1 (elements 4-7)
+            l[0] * r[4] + l[4] * r[5] + l[8] * r[6] + l[12] * r[7],   // [0,1]
+            l[1] * r[4] + l[5] * r[5] + l[9] * r[6] + l[13] * r[7],   // [1,1]
+            l[2] * r[4] + l[6] * r[5] + l[10] * r[6] + l[14] * r[7],  // [2,1]
+            l[3] * r[4] + l[7] * r[5] + l[11] * r[6] + l[15] * r[7],  // [3,1]
 
-            lhs.get(2, 0) * rhs.get(0, 0) + lhs.get(2, 1) * rhs.get(1, 0) + lhs.get(2, 2) * rhs.get(2, 0) + lhs.get(2, 3) * rhs.get(3, 0),
-            lhs.get(2, 0) * rhs.get(0, 1) + lhs.get(2, 1) * rhs.get(1, 1) + lhs.get(2, 2) * rhs.get(2, 1) + lhs.get(2, 3) * rhs.get(3, 1),
-            lhs.get(2, 0) * rhs.get(0, 2) + lhs.get(2, 1) * rhs.get(1, 2) + lhs.get(2, 2) * rhs.get(2, 2) + lhs.get(2, 3) * rhs.get(3, 2),
-            lhs.get(2, 0) * rhs.get(0, 3) + lhs.get(2, 1) * rhs.get(1, 3) + lhs.get(2, 2) * rhs.get(2, 3) + lhs.get(2, 3) * rhs.get(3, 3),
+            // Column 2 (elements 8-11)
+            l[0] * r[8] + l[4] * r[9] + l[8] * r[10] + l[12] * r[11], // [0,2]
+            l[1] * r[8] + l[5] * r[9] + l[9] * r[10] + l[13] * r[11], // [1,2]
+            l[2] * r[8] + l[6] * r[9] + l[10] * r[10] + l[14] * r[11], // [2,2]
+            l[3] * r[8] + l[7] * r[9] + l[11] * r[10] + l[15] * r[11], // [3,2]
 
-            lhs.get(3, 0) * rhs.get(0, 0) + lhs.get(3, 1) * rhs.get(1, 0) + lhs.get(3, 2) * rhs.get(2, 0) + lhs.get(3, 3) * rhs.get(3, 0),
-            lhs.get(3, 0) * rhs.get(0, 1) + lhs.get(3, 1) * rhs.get(1, 1) + lhs.get(3, 2) * rhs.get(2, 1) + lhs.get(3, 3) * rhs.get(3, 1),
-            lhs.get(3, 0) * rhs.get(0, 2) + lhs.get(3, 1) * rhs.get(1, 2) + lhs.get(3, 2) * rhs.get(2, 2) + lhs.get(3, 3) * rhs.get(3, 2),
-            lhs.get(3, 0) * rhs.get(0, 3) + lhs.get(3, 1) * rhs.get(1, 3) + lhs.get(3, 2) * rhs.get(2, 3) + lhs.get(3, 3) * rhs.get(3, 3)
+            // Column 3 (elements 12-15)
+            l[0] * r[12] + l[4] * r[13] + l[8] * r[14] + l[12] * r[15], // [0,3]
+            l[1] * r[12] + l[5] * r[13] + l[9] * r[14] + l[13] * r[15], // [1,3]
+            l[2] * r[12] + l[6] * r[13] + l[10] * r[14] + l[14] * r[15], // [2,3]
+            l[3] * r[12] + l[7] * r[13] + l[11] * r[14] + l[15] * r[15]  // [3,3]
         );
     }
 
@@ -756,6 +787,8 @@ namespace Uma_Math
         );
     }
 
+    // I don't have Vector4D to perform Matrix4x4 and Vector4D multiplication... yet?
+
     // Inverse operations (for floating point types)
     template <typename T>
     auto inverse(const Matrix2x2<T>& mat) -> std::enable_if_t<std::is_floating_point_v<T>, Matrix2x2<T>>
@@ -794,6 +827,133 @@ namespace Uma_Math
             (mat.get(0, 1) * mat.get(2, 0) - mat.get(0, 0) * mat.get(2, 1)) * inv_det,
             (mat.get(0, 0) * mat.get(1, 1) - mat.get(0, 1) * mat.get(1, 0)) * inv_det
         );
+    }
+
+    template <typename T>
+    auto inverse(const Matrix4x4<T>& m) -> std::enable_if_t<std::is_floating_point_v<T>, Matrix4x4<T>>
+    {
+        T det = m.determinant();
+        if (std::abs(det) < std::numeric_limits<T>::epsilon())
+        {
+            throw std::runtime_error("Matrix is not invertible, determinant is zero");
+        }
+
+        T inv_det = T{ 1 } / det;
+        Matrix4x4<T> result;
+        result.set(0, 0, (m.get(1, 1) * (m.get(2, 2) * m.get(3, 3) - m.get(2, 3) * m.get(3, 2)) -
+            m.get(1, 2) * (m.get(2, 1) * m.get(3, 3) - m.get(2, 3) * m.get(3, 1)) +
+            m.get(1, 3) * (m.get(2, 1) * m.get(3, 2) - m.get(2, 2) * m.get(3, 1))) * inv_det);
+
+        result.set(1, 0, -(m.get(1, 0) * (m.get(2, 2) * m.get(3, 3) - m.get(2, 3) * m.get(3, 2)) -
+            m.get(1, 2) * (m.get(2, 0) * m.get(3, 3) - m.get(2, 3) * m.get(3, 0)) +
+            m.get(1, 3) * (m.get(2, 0) * m.get(3, 2) - m.get(2, 2) * m.get(3, 0))) * inv_det);
+
+        result.set(2, 0, (m.get(1, 0) * (m.get(2, 1) * m.get(3, 3) - m.get(2, 3) * m.get(3, 1)) -
+            m.get(1, 1) * (m.get(2, 0) * m.get(3, 3) - m.get(2, 3) * m.get(3, 0)) +
+            m.get(1, 3) * (m.get(2, 0) * m.get(3, 1) - m.get(2, 1) * m.get(3, 0))) * inv_det);
+
+        result.set(3, 0, -(m.get(1, 0) * (m.get(2, 1) * m.get(3, 2) - m.get(2, 2) * m.get(3, 1)) -
+            m.get(1, 1) * (m.get(2, 0) * m.get(3, 2) - m.get(2, 2) * m.get(3, 0)) +
+            m.get(1, 2) * (m.get(2, 0) * m.get(3, 1) - m.get(2, 1) * m.get(3, 0))) * inv_det);
+
+        result.set(0, 1, -(m.get(0, 1) * (m.get(2, 2) * m.get(3, 3) - m.get(2, 3) * m.get(3, 2)) -
+            m.get(0, 2) * (m.get(2, 1) * m.get(3, 3) - m.get(2, 3) * m.get(3, 1)) +
+            m.get(0, 3) * (m.get(2, 1) * m.get(3, 2) - m.get(2, 2) * m.get(3, 1))) * inv_det);
+
+        result.set(1, 1, (m.get(0, 0) * (m.get(2, 2) * m.get(3, 3) - m.get(2, 3) * m.get(3, 2)) -
+            m.get(0, 2) * (m.get(2, 0) * m.get(3, 3) - m.get(2, 3) * m.get(3, 0)) +
+            m.get(0, 3) * (m.get(2, 0) * m.get(3, 2) - m.get(2, 2) * m.get(3, 0))) * inv_det);
+
+        result.set(2, 1, -(m.get(0, 0) * (m.get(2, 1) * m.get(3, 3) - m.get(2, 3) * m.get(3, 1)) -
+            m.get(0, 1) * (m.get(2, 0) * m.get(3, 3) - m.get(2, 3) * m.get(3, 0)) +
+            m.get(0, 3) * (m.get(2, 0) * m.get(3, 1) - m.get(2, 1) * m.get(3, 0))) * inv_det);
+
+        result.set(3, 1, (m.get(0, 0) * (m.get(2, 1) * m.get(3, 2) - m.get(2, 2) * m.get(3, 1)) -
+            m.get(0, 1) * (m.get(2, 0) * m.get(3, 2) - m.get(2, 2) * m.get(3, 0)) +
+            m.get(0, 2) * (m.get(2, 0) * m.get(3, 1) - m.get(2, 1) * m.get(3, 0))) * inv_det);
+
+        result.set(0, 2, (m.get(0, 1) * (m.get(1, 2) * m.get(3, 3) - m.get(1, 3) * m.get(3, 2)) -
+            m.get(0, 2) * (m.get(1, 1) * m.get(3, 3) - m.get(1, 3) * m.get(3, 1)) +
+            m.get(0, 3) * (m.get(1, 1) * m.get(3, 2) - m.get(1, 2) * m.get(3, 1))) * inv_det);
+
+        result.set(1, 2, -(m.get(0, 0) * (m.get(1, 2) * m.get(3, 3) - m.get(1, 3) * m.get(3, 2)) -
+            m.get(0, 2) * (m.get(1, 0) * m.get(3, 3) - m.get(1, 3) * m.get(3, 0)) +
+            m.get(0, 3) * (m.get(1, 0) * m.get(3, 2) - m.get(1, 2) * m.get(3, 0))) * inv_det);
+
+        result.set(2, 2, (m.get(0, 0) * (m.get(1, 1) * m.get(3, 3) - m.get(1, 3) * m.get(3, 1)) -
+            m.get(0, 1) * (m.get(1, 0) * m.get(3, 3) - m.get(1, 3) * m.get(3, 0)) +
+            m.get(0, 3) * (m.get(1, 0) * m.get(3, 1) - m.get(1, 1) * m.get(3, 0))) * inv_det);
+
+        result.set(3, 2, -(m.get(0, 0) * (m.get(1, 1) * m.get(3, 2) - m.get(1, 2) * m.get(3, 1)) -
+            m.get(0, 1) * (m.get(1, 0) * m.get(3, 2) - m.get(1, 2) * m.get(3, 0)) +
+            m.get(0, 2) * (m.get(1, 0) * m.get(3, 1) - m.get(1, 1) * m.get(3, 0))) * inv_det);
+
+        result.set(0, 3, -(m.get(0, 1) * (m.get(1, 2) * m.get(2, 3) - m.get(1, 3) * m.get(2, 2)) -
+            m.get(0, 2) * (m.get(1, 1) * m.get(2, 3) - m.get(1, 3) * m.get(2, 1)) +
+            m.get(0, 3) * (m.get(1, 1) * m.get(2, 2) - m.get(1, 2) * m.get(2, 1))) * inv_det);
+
+        result.set(1, 3, (m.get(0, 0) * (m.get(1, 2) * m.get(2, 3) - m.get(1, 3) * m.get(2, 2)) -
+            m.get(0, 2) * (m.get(1, 0) * m.get(2, 3) - m.get(1, 3) * m.get(2, 0)) +
+            m.get(0, 3) * (m.get(1, 0) * m.get(2, 2) - m.get(1, 2) * m.get(2, 0))) * inv_det);
+
+        result.set(2, 3, -(m.get(0, 0) * (m.get(1, 1) * m.get(2, 3) - m.get(1, 3) * m.get(2, 1)) -
+            m.get(0, 1) * (m.get(1, 0) * m.get(2, 3) - m.get(1, 3) * m.get(2, 0)) +
+            m.get(0, 3) * (m.get(1, 0) * m.get(2, 1) - m.get(1, 1) * m.get(2, 0))) * inv_det);
+
+        result.set(3, 3, (m.get(0, 0) * (m.get(1, 1) * m.get(2, 2) - m.get(1, 2) * m.get(2, 1)) -
+            m.get(0, 1) * (m.get(1, 0) * m.get(2, 2) - m.get(1, 2) * m.get(2, 0)) +
+            m.get(0, 2) * (m.get(1, 0) * m.get(2, 1) - m.get(1, 1) * m.get(2, 0))) * inv_det);
+
+        return result;
+    }
+
+    // GLM-style transformation matrices
+    template <typename T>
+    Matrix4x4<T> translate(const Matrix4x4<T>& mat, const Vector3D<T>& v)
+    {
+        Matrix4x4<T> result(mat);
+        result.setColumn(3, mat.column(0) * v.x + mat.column(1) * v.y + mat.column(2) * v.z + mat.column(3));
+        return result;
+    }
+
+    template <typename T>
+    Matrix4x4<T> scale(const Matrix4x4<T>& mat, const Vector3D<T>& v)
+    {
+        Matrix4x4<T> result;
+        result.setColumn(0, mat.column(0) * v.x);
+        result.setColumn(1, mat.column(1) * v.y);
+        result.setColumn(2, mat.column(2) * v.z);
+        result.setColumn(3, mat.column(3));
+        return result;
+    }
+
+    template <typename T>
+    Matrix4x4<T> rotate(const Matrix4x4<T>& mat, T angle, const Vector3D<T>& axis)
+    {
+        T const c = std::cos(angle);
+        T const s = std::sin(angle);
+
+        Vector3D<T> temp = normalize(axis) * (T{ 1 } - c);
+
+        Matrix4x4<T> rotation;
+        rotation.set(0, 0, c + temp.x * axis.x);
+        rotation.set(0, 1, temp.x * axis.y + s * axis.z);
+        rotation.set(0, 2, temp.x * axis.z - s * axis.y);
+
+        rotation.set(1, 0, temp.y * axis.x - s * axis.z);
+        rotation.set(1, 1, c + temp.y * axis.y);
+        rotation.set(1, 2, temp.y * axis.z + s * axis.x);
+
+        rotation.set(2, 0, temp.z * axis.x + s * axis.y);
+        rotation.set(2, 1, temp.z * axis.y - s * axis.x);
+        rotation.set(2, 2, c + temp.z * axis.z);
+
+        Matrix4x4<T> result;
+        result.setColumn(0, mat.column(0) * rotation.get(0, 0) + mat.column(1) * rotation.get(0, 1) + mat.column(2) * rotation.get(0, 2));
+        result.setColumn(1, mat.column(0) * rotation.get(1, 0) + mat.column(1) * rotation.get(1, 1) + mat.column(2) * rotation.get(1, 2));
+        result.setColumn(2, mat.column(0) * rotation.get(2, 0) + mat.column(1) * rotation.get(2, 1) + mat.column(2) * rotation.get(2, 2));
+        result.setColumn(3, mat.column(3));
+        return result;
     }
 
     // Stream operators
