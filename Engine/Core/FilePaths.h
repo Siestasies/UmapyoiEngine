05c646add4d449654b13f400644d56b104026cd6
beatsmoon <wai.men.leong@gmail.com>
2025-09-27 20:39:49 +0800

    added jed math

---
+++ Engine/Core/FilePaths.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <string>
+
+namespace FilePaths {
+    // Assets
+    constexpr const char* TEXTURES_DIR = "Assets/";
+    constexpr const char* AUDIO_DIR = "Assets/Audios";
+
+    // Scenes
+    constexpr const char* SCENES_DIR = "Assets/Scenes/";
+    constexpr const char* PREFAB_DIR = "Assets/Prefabs/";
+
+    //constexpr const char* MODELS_DIR = "Assets/Models/";
+    //constexpr const char* MATERIALS_DIR = "Assets/Materials/";
+    //constexpr const char* SHADERS_DIR = "Assets/Shaders/";
+
+    //// Specific models
+    //constexpr const char* TREE_MODEL = "Assets/Models/tree.obj";
+    //constexpr const char* PLAYER_MODEL = "Assets/Models/player.obj";
+
+    //// Materials
+    //constexpr const char* LEAF_MATERIAL = "Assets/Materials/leaf.mat";
+    //constexpr const char* PLAYER_MATERIAL = "Assets/Materials/player.mat";
+
+    //// Shaders
+    //constexpr const char* BASIC_VERTEX_SHADER = "Assets/Shaders/basic.vert";
+    //constexpr const char* BASIC_FRAGMENT_SHADER = "Assets/Shaders/basic.frag";
+
+    //// Save/Load
+    //constexpr const char* SCENE_SAVE_FILE = "Saves/scene.json";
+}
