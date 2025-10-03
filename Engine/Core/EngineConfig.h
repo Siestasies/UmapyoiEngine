438620c842e4a2a31721abcf0ef0229672cb083e
beatsmoon <wai.men.leong@gmail.com>
2025-10-03 16:19:50 +0800

    Update EngineConfig.h

--- Engine/Core/EngineConfig.h
+++ Engine/Core/EngineConfig.h
@@ -1,3 +1,22 @@
+/*!
+\file   EngineConfig.h
+\par    Project: GAM200
+\par    Course: CSD2401
+\par    Section A
+\par    Software Engineering Project 3
+
+\author Leong Wai Men (100%)
+\par    E-mail: waimen.leong@digipen.edu
+\par    DigiPen login: waimen.leong
+
+/*!
+\brief
+A container to store configs
+
+All content (C) 2025 DigiPen Institute of Technology Singapore.
+All rights reserved.
+*/
+
 #pragma once
 #include <string>
 
