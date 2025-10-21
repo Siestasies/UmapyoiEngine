bb593f7fb5604984a918dce65cef91c7cb9c2091
ragingknight <kohkaiyang@gmail.com>
2025-10-22 01:41:05 +0800

    started the pathfinding functions

---
+++ Engine/AI/pathfinding.cpp
@@ -0,0 +1,19 @@
+#include "pathfinding.hpp"
+
+Uma_Engine::Pathfinding::Pathfinding(){
+	//read the map 
+}
+
+Uma_Engine::Pathfinding::~Pathfinding(){
+}
+
+bool Uma_Engine::Pathfinding::isValid(vec2 pos){
+	//check 2d array of map
+	return false;
+}
+
+vec2 Uma_Engine::Pathfinding::path(vec2 pos, vec2 dest){
+
+
+	return vec2();
+}
---
+++ Engine/AI/pathfinding.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include "../Math/Vector.h"
+#include <vector>
+
+using vec2 = Uma_Math::Vector2D<float>;
+
+namespace Uma_Engine {
+
+	struct Node {
+		int x, y;
+		float g, h, f;
+		Node* parent;
+	};
+
+	class Pathfinding {
+	public:
+		Pathfinding();
+		~Pathfinding();
+
+
+		bool isValid(vec2 pos);
+		vec2 path(vec2 pos, vec2 dest);
+
+	private:
+		std::vector<std::vector<int>> map;
+	};
+
+
+}
