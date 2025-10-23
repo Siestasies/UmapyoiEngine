acd10b07df2d90ecdd8c960840b5c609ff40566e
beatsmoon <wai.men.leong@gmail.com>
2025-10-23 22:58:22 +0800

    Update IMGUIEvents.h

--- Engine/Events/IMGUIEvents.h
+++ Engine/Events/IMGUIEvents.h
@@ -31,129 +31,129 @@ All rights reserved.
 
 namespace Uma_Engine
 {
-	class SpawnEntityRequestEvent : public Event
-	{
-	public:
-		SpawnEntityRequestEvent() { priority = Priority::Normal; }
-	};
+    class SpawnEntityRequestEvent : public Event
+    {
+    public:
+        SpawnEntityRequestEvent() { priority = Priority::Normal; }
+    };
 
-	class DestroyEntityRequestEvent : public Event
-	{
-	public:
-		DestroyEntityRequestEvent(Uma_ECS::Entity entityId) : entityId(entityId) { priority = Priority::High; }
-	public:
-		Uma_ECS::Entity entityId;
-	};
+    class DestroyEntityRequestEvent : public Event
+    {
+    public:
+        DestroyEntityRequestEvent(Uma_ECS::Entity entityId) : entityId(entityId) { priority = Priority::High; }
+    public:
+        Uma_ECS::Entity entityId;
+    };
 
-	class StressTestRequestEvent : public Event
-	{
-	public:
-			StressTestRequestEvent(){ priority = Priority::High; }
-	};
+    class StressTestRequestEvent : public Event
+    {
+    public:
+        StressTestRequestEvent() { priority = Priority::High; }
+    };
 
-	class ShowEntityInVPRequestEvent : public Event
-	{
-	public:
-			ShowEntityInVPRequestEvent() { priority = Priority::High; }
-	};
+    class ShowEntityInVPRequestEvent : public Event
+    {
+    public:
+        ShowEntityInVPRequestEvent() { priority = Priority::High; }
+    };
 
-	class ChangeEnemyRotRequestEvent : public Event
-	{
-	public:
-			float rot;
-			ChangeEnemyRotRequestEvent(float rot) : rot(rot) { priority = Priority::High; }
-	};
+    class ChangeEnemyRotRequestEvent : public Event
+    {
+    public:
+        float rot;
+        ChangeEnemyRotRequestEvent(float rot) : rot(rot) { priority = Priority::High; }
+    };
 
-	class ChangeEnemyXposRequestEvent : public Event
-	{
-	public:
-			float xpos;
-			ChangeEnemyXposRequestEvent(float xpos) : xpos(xpos) { priority = Priority::High; }
-	};
+    class ChangeEnemyXposRequestEvent : public Event
+    {
+    public:
+        float xpos;
+        ChangeEnemyXposRequestEvent(float xpos) : xpos(xpos) { priority = Priority::High; }
+    };
 
-	class LoadPrefabRequestEvent : public Event
-	{
-	public:
-			
-			LoadPrefabRequestEvent() { priority = Priority::High; }
-	};
+    class LoadPrefabRequestEvent : public Event
+    {
+    public:
 
-	class ChangeEnemyScaleRequestEvent : public Event
-	{
-	public:
-			float scale;
-			ChangeEnemyScaleRequestEvent(float scale) : scale(scale) { priority = Priority::High; }
-	};
+        LoadPrefabRequestEvent() { priority = Priority::High; }
+    };
 
-	class ShowBBoxRequestEvent : public Event
-	{
-	public:
-			bool show;
-			ShowBBoxRequestEvent(bool show) : show(show) { priority = Priority::High; }
-	};
+    class ChangeEnemyScaleRequestEvent : public Event
+    {
+    public:
+        float scale;
+        ChangeEnemyScaleRequestEvent(float scale) : scale(scale) { priority = Priority::High; }
+    };
 
-	class CloneEntityRequestEvent : public Event
-	{
-	public:
-		CloneEntityRequestEvent(Uma_ECS::Entity entityId) : entityId(entityId) { priority = Priority::High; }
-	public:
-		Uma_ECS::Entity entityId;
-	};
+    class ShowBBoxRequestEvent : public Event
+    {
+    public:
+        bool show;
+        ShowBBoxRequestEvent(bool show) : show(show) { priority = Priority::High; }
+    };
 
-	class QueryActiveEntitiesEvent : public Event
-	{
-	public:
-		QueryActiveEntitiesEvent()
-		{
-			priority = Priority::Critical;
-		}
+    class CloneEntityRequestEvent : public Event
+    {
+    public:
+        CloneEntityRequestEvent(Uma_ECS::Entity entityId) : entityId(entityId) { priority = Priority::High; }
+    public:
+        Uma_ECS::Entity entityId;
+    };
 
-	public:
-		mutable int mActiveEntityCnt = 0;
-		//mutable std::vector<Uma_ECS::Entity> entities;
-	};
+    class QueryActiveEntitiesEvent : public Event
+    {
+    public:
+        QueryActiveEntitiesEvent()
+        {
+            priority = Priority::Critical;
+        }
 
-	class AddComponentRequestEvent : public Event
-	{
-	public:
-		AddComponentRequestEvent(Uma_ECS::Entity entityId, std::string componentType) : entityId(entityId), componentType(componentType) { priority = Priority::Normal; }
+    public:
+        mutable int mActiveEntityCnt = 0;
+        //mutable std::vector<Uma_ECS::Entity> entities;
+    };
 
-	public:
-		Uma_ECS::Entity entityId;
-		std::string componentType;
-	};
+    class AddComponentRequestEvent : public Event
+    {
+    public:
+        AddComponentRequestEvent(Uma_ECS::Entity entityId, std::string componentType) : entityId(entityId), componentType(componentType) { priority = Priority::Normal; }
 
-	class RemoveComponentRequestEvent : public Event
-	{
-	public:
-		RemoveComponentRequestEvent(Uma_ECS::Entity entityId, std::string componentType) : entityId(entityId), componentType(componentType) { priority = Priority::Normal; }
+    public:
+        Uma_ECS::Entity entityId;
+        std::string componentType;
+    };
 
-	public:
-		Uma_ECS::Entity entityId;
-		std::string componentType;
-	};
+    class RemoveComponentRequestEvent : public Event
+    {
+    public:
+        RemoveComponentRequestEvent(Uma_ECS::Entity entityId, std::string componentType) : entityId(entityId), componentType(componentType) { priority = Priority::Normal; }
 
-	class ClearSceneRequestEvent : public Event
-	{
-	public:
-		ClearSceneRequestEvent() { priority = Priority::High; }
-	};
+    public:
+        Uma_ECS::Entity entityId;
+        std::string componentType;
+    };
 
-	class SaveSceneRequestEvent : public Event
-	{
-	public:
-		SaveSceneRequestEvent(const std::string& filepath = "") : filepath(filepath) { priority = Priority::Normal; }
+    class ClearSceneRequestEvent : public Event
+    {
+    public:
+        ClearSceneRequestEvent() { priority = Priority::High; }
+    };
 
-	public:
-		std::string filepath;
-	};
+    class SaveSceneRequestEvent : public Event
+    {
+    public:
+        SaveSceneRequestEvent(const std::string& filepath = "") : filepath(filepath) { priority = Priority::Normal; }
 
-	class LoadSceneRequestEvent : public Event
-	{
-	public:
-		LoadSceneRequestEvent(const std::string& filepath) : filepath(filepath) { priority = Priority::High; }
+    public:
+        std::string filepath;
+    };
 
-	public:
-		std::string filepath;
-	};
+    class LoadSceneRequestEvent : public Event
+    {
+    public:
+        LoadSceneRequestEvent(const std::string& filepath) : filepath(filepath) { priority = Priority::High; }
+
+    public:
+        std::string filepath;
+    };
 }
