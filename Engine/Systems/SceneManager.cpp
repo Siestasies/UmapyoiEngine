708a1b95940d8c854b451f7a1e7ecb3ea2f48ef0
beatsmoon <wai.men.leong@gmail.com>
2025-11-27 21:07:59 +0800

    FIX LAOD SCENE ISSUE

--- Engine/Systems/SceneManager.cpp
+++ Engine/Systems/SceneManager.cpp
@@ -106,8 +106,8 @@ namespace Uma_Engine
         pEventSystem->Subscribe<LoadSceneRequestEvent, SceneManager>(
             [this](const LoadSceneRequestEvent& e) {
                 LoadScene(e.name, false);
-                m_UseEditorCamera = false;
-                playMode = PLAYMODE::PM_STOP;
+                m_UseEditorCamera = e.load_n_play ? true : false;
+                playMode = e.load_n_play ? PLAYMODE::PM_PLAY : PLAYMODE::PM_STOP;
             }
         );
 
