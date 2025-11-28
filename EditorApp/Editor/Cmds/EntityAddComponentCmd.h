8745c8cb670611ed276dbe2e4c629b104db6face
Siestasies <bitterhoronigai@gmail.com>
2025-11-29 00:33:07 +0800

    comment

--- EditorApp/Editor/Cmds/EntityAddComponentCmd.h
+++ EditorApp/Editor/Cmds/EntityAddComponentCmd.h
@@ -1,3 +1,22 @@
+/*!
+\file   EntityAddComponentCmd.h
+\par    Project: GAM200
+\par    Course: CSD2401
+\par    Section A
+\par    Software Engineering Project 3
+
+\author Leong Wai Men (100%)
+\par    E-mail: waimen.leong@digipen.edu
+\par    DigiPen login: waimen.leong
+
+\brief
+Defines the EntityAddComponentCmd class, a templated command used by the Editor's
+Undo/Redo system to handle adding components to entities.
+
+All content (C) 2025 DigiPen Institute of Technology Singapore.
+All rights reserved.
+*/
+
 #pragma once
 
 #include "Editor/Core/Command.h"
@@ -14,7 +33,7 @@ namespace Uma_Editor
         Uma_ECS::Entity mEntity;
         T mComponentData;       // The data to add (e.g., default values)
         std::string mDescription;
-        bool mWasAdded;         // Track if we actually added it (safety for Undo)
+        bool mWasAdded;         // Track if actually added it
 
     public:
         EntityAddComponentCmd(
@@ -31,6 +50,11 @@ namespace Uma_Editor
         {
         }
 
+        /**
+         * \brief Executes the add component operation.
+         * * Checks if the entity exists and does NOT already have the component before adding.
+         * Sets mWasAdded to true if successful.
+         */
         void Execute() override
         {
             // Only add if the entity exists and DOES NOT have the component
@@ -45,15 +69,23 @@ namespace Uma_Editor
             }
         }
 
+        /**
+         * \brief Undoes the add operation by removing the component.
+         * * Only removes the component if this specific command instance was responsible
+         * for adding it (mWasAdded is true).
+         */
         void Undo() override
         {
-            // Only remove if we were the ones who added it
             if (mWasAdded && pCoordinator->HasActiveEntity(mEntity) && pCoordinator->HasComponent<T>(mEntity))
             {
                 pCoordinator->RemoveComponent<T>(mEntity);
             }
         }
 
+        /**
+         * \brief Gets the description of the command.
+         * \return The string description passed in the constructor.
+         */
         std::string GetDescription() override
         {
             return mDescription;
--- EditorApp/Editor/Cmds/EntityRemoveComponentCmd.h
+++ EditorApp/Editor/Cmds/EntityRemoveComponentCmd.h
@@ -1,3 +1,22 @@
+/*!
+\file   EntityRemoveComponentCmd.h
+\par    Project: GAM200
+\par    Course: CSD2401
+\par    Section A
+\par    Software Engineering Project 3
+
+\author Leong Wai Men (100%)
+\par    E-mail: waimen.leong@digipen.edu
+\par    DigiPen login: waimen.leong
+
+\brief
+Defines the EntityRemoveComponentCmd class, a templated command used by the Editor's
+Undo/Redo system to handle removing components from entities.
+
+All content (C) 2025 DigiPen Institute of Technology Singapore.
+All rights reserved.
+*/
+
 #pragma once
 
 #include "Editor/Core/Command.h"
@@ -13,7 +32,7 @@ namespace Uma_Editor
         Uma_ECS::Entity mEntity;
         T mBackupData;          // Stores the data BEFORE removal
         std::string mDescription;
-        bool mWasRemoved;       // Track if we actually removed it
+        bool mWasRemoved;       // Track if actually removed it
 
     public:
         EntityRemoveComponentCmd(
@@ -33,6 +52,11 @@ namespace Uma_Editor
             }
         }
 
+        /**
+         * \brief Executes the remove component operation.
+         * * Removes the component from the entity if it exists.
+         * Sets mWasRemoved to true if successful.
+         */
         void Execute() override
         {
             if (pCoordinator->HasActiveEntity(mEntity) && pCoordinator->HasComponent<T>(mEntity))
@@ -46,6 +70,11 @@ namespace Uma_Editor
             }
         }
 
+        /**
+         * \brief Undoes the remove operation by restoring the component.
+         * * Re-adds the component using the backed-up data (mBackupData) if it was
+         * successfully removed by this command instance.
+         */
         void Undo() override
         {
             // If we successfully removed it, add it back with the backup data
@@ -55,6 +84,10 @@ namespace Uma_Editor
             }
         }
 
+        /**
+         * \brief Gets the description of the command.
+         * \return The string description passed in the constructor.
+         */
         std::string GetDescription() override
         {
             return mDescription;
