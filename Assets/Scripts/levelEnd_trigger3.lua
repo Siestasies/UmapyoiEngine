561567b4bc04a925ba41146ca20720fd9961ab8a
Nako <rasidshahir@gmail.com>
2026-02-06 15:54:14 +0800

    dfgdfgdgfdgdgdgdg

--- Assets/Scripts/levelEnd_trigger3.lua
+++ Assets/Scripts/levelEnd_trigger3.lua
@@ -1,33 +1,26 @@
-local levelEndState
+local levelEnd
 local fadeState
 local time = 1
-local loadNextScene = false
-
--- when player step on trigger
-function OnTriggerEnter(other)
-    -- trigger fading
-    if HasPlayerOn(other) then
-        loadNextScene = true
-    end
-end
 
 function Start()
-    levelEndState = require("levelEndState")
+    levelEnd = false
     fadeState = require("fadeState")
-    levelEndState.setLevelEnd(false)
+    levelEndState = require("levelEndState")
 end
 
 function Update(dt)
-    -- if no enemies, set level end flag
-    local enemies = FindEntitiesWithComponent("Enemy")
-    -- set to 1 for debugging
-    if enemies == 0 then
-        levelEndState.setLevelEnd(true)
-    end
-
-    -- timer for when want to load next scene
-    -- after player step on trigger
-    if loadNextScene then
+    if levelEnd then
         levelEndState.setLevelEndMenu(true)
+    end
+end
+
+-- when player step on trigger
+function OnTriggerEnter(other, triggerOwner)
+    -- trigger fading
+    if HasPlayerOn(triggerOwner) or HasPlayerOn(other) then
+        local enemyCount = CountEntitiesWithComponent("Enemy")
+        if enemyCount <= 0 then
+            levelEnd = true
+        end
     end
 end
