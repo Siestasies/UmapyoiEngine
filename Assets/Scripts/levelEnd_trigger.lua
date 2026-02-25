05d9ada689ac733985a51e14944144c64e5fc091
ragingknight <kohkaiyang@gmail.com>
2026-02-25 20:34:54 +0800

    testing some stuff

--- Assets/Scripts/levelEnd_trigger.lua
+++ Assets/Scripts/levelEnd_trigger.lua
@@ -25,6 +25,7 @@ function OnTriggerEnter(other, triggerOwner)
         local enemyCount = CountEntitiesWithComponent("Enemy")
         if enemyCount <= 0 then
             levelEnd = true
+            GetAudioComponent():playFaded(EntityID,"MainMenuBGM",1.5)
         end
     end
 end
